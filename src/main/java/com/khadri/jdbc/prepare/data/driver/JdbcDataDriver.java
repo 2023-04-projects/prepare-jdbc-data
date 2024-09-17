c313a55c155c49f11448ae2d40ff0215d75ae860
sgreddy123 <151907854+sgreddy123@users.noreply.github.com>
2024-09-17 14:11:42 +0530

    Customer module (#1)
    * Customer module
    
    * modified Customer files
    
    * modified customer module
    
    * Updated Customer Module

--- src/main/java/com/khadri/jdbc/prepare/data/driver/JdbcDataDriver.java
+++ src/main/java/com/khadri/jdbc/prepare/data/driver/JdbcDataDriver.java
@@ -3,17 +3,22 @@ package com.khadri.jdbc.prepare.data.driver;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import com.khadri.jdbc.prepare.data.customer.dao.CustomerInsertDao;
+import com.khadri.jdbc.prepare.data.customer.processor.CustomerDataProcessor;
 import com.khadri.jdbc.prepare.data.driver.types.DriverTypes;
 import com.khadri.jdbc.prepare.data.student.dao.StudentInsertDao;
 import com.khadri.jdbc.prepare.data.student.processor.StudentDataProcessor;
 
 public class JdbcDataDriver {
 
-	private Scanner scanner;
-	private StudentInsertDao insertDao;
+	public Scanner scanner;
+	public StudentInsertDao stdDao;
+	public CustomerInsertDao custDao;
 	{
 		scanner = new Scanner(System.in);
-		insertDao = new StudentInsertDao();
+		stdDao = new StudentInsertDao();
+		custDao = new CustomerInsertDao();
+
 	}
 
 	public static void main(String[] args) {
@@ -25,6 +30,7 @@ public class JdbcDataDriver {
 		});
 
 		JdbcDataDriver csvDataDriver = new JdbcDataDriver();
+
 		csvDataDriver.process();
 	}
 
@@ -35,20 +41,32 @@ public class JdbcDataDriver {
 		switch (dataProcessId) {
 		case 1:
 
-			System.out.println("How many records you want to insert ? : ");
+			System.out.println("How many records do you want to insert ? : ");
 			int recordCount = scanner.nextInt();
 
-			StudentDataProcessor stdProcessor = new StudentDataProcessor(scanner,insertDao);
-
+			StudentDataProcessor stdProcessor = new StudentDataProcessor(scanner,stdDao);
+			int count = 1;
 			for (int i = 0; i < recordCount; i++) {
-				stdProcessor.process(dataProcessId);
+				stdProcessor.process(count);
+				count++;
 			}
-
 			break;
+		case 3:
+			System.out.println("How many records do you want to insert ? : ");
+			int custCount = scanner.nextInt();
 
+			CustomerDataProcessor custProcessor = new CustomerDataProcessor(scanner,custDao);
+			int rowCount = 1;
+			for (int i = 0; i < custCount; i++)
+				try {
+					custProcessor.process(rowCount);
+					rowCount++;
+				} catch (Exception e) {
+					System.out.println("Exception occours:" + e);
+				}
+			break;
 		default:
 			break;
 		}
-
 	}
 }
