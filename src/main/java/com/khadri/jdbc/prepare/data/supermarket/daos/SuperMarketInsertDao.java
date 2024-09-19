7406dcc47fc13700f579ef890aaf20e1ec227a6a
shaikroohi <roohishaik128@gmail.com>
2024-09-19 19:47:16 +0530

    Added select method

--- src/main/java/com/khadri/jdbc/prepare/data/supermarket/daos/SuperMarketInsertDao.java
+++ src/main/java/com/khadri/jdbc/prepare/data/supermarket/daos/SuperMarketInsertDao.java
@@ -2,13 +2,14 @@ package com.khadri.jdbc.prepare.data.supermarket.daos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.khadri.jdbc.prepare.data.supermarket.dao.model.SuperMarket;
 
 public class SuperMarketInsertDao {
 
-	public static void insertSuperMarketData(SuperMarket market){
+	public static void insertSuperMarketData(SuperMarket market) {
 
 		PreparedStatement pstmt = null;
 		try {
@@ -25,11 +26,43 @@ public class SuperMarketInsertDao {
 			pstmt.executeUpdate();
 
 		} catch (SQLException e) {
-			System.out.println("Exception Occured : " + e);
-		}finally {
+			System.out.println("SuperMarKetInsertException Occured : " + e);
+		} finally {
 			JdbcConnectionUtil.closeResources();
 		}
-
 	}
 
+	public void selecttSuperMarketData(SuperMarket market) {
+		try {
+			Connection conn = JdbcConnectionUtil.getConnection();
+
+			PreparedStatement pstmt = conn.prepareStatement("Select * from superMarket");
+			ResultSet resultset = pstmt.executeQuery();
+			
+			while(resultset.next()) {
+				
+				System.out.println("Cust_Name : " + resultset.getString(1) 
+				+ "Cust_Id : " + resultset.getInt(2) + "Cust_Price : " + resultset.getDouble(3)
+				+ "Cust_Qty" + resultset.getInt(4) + "Total_Amt : " + resultset.getDouble(5));
+				
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcConnectionUtil.closeResources();
+		}
+	}
 }
+
+
+
+
+/*
+ * if()
+ * ResultSet resultSet = stmt.getResultSet();
+while (resultSet.next()) {
+System.out.println(resultSet.getString(1));
+}
+
+*/
