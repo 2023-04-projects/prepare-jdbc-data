d7e39b6ea5b20c1f43ba296b3964b82c0fa5bd5b
manvithagali52 <manvithag52@gmail.com>
2024-09-22 11:28:05 +0530

    implemented update and delete modules

--- src/main/java/com/khadri/jdbc/prepare/data/driver/JdbcDataDriver.java
+++ src/main/java/com/khadri/jdbc/prepare/data/driver/JdbcDataDriver.java
@@ -18,14 +18,12 @@ import com.khadri.jdbc.prepare.data.supermarket.processor.SuperMarketDataProcess
 
 public class JdbcDataDriver {
 	private Scanner scanner;
-
 	private EmployeeDao empDao;;
-
 	private CustomerDao custDao;
-
 	private MovieDao movieDao;
 	private MobileDao mobileDao;
 	private SuperMarketDao superMarketDao;
+	private EmployeeDataProcessor empProcessor;
 
 	{
 		scanner = new Scanner(System.in);
@@ -34,9 +32,7 @@ public class JdbcDataDriver {
 		custDao = new CustomerDao();
 		movieDao = new MovieDao();
 		superMarketDao = new SuperMarketDao();
-		superMarketDao = new SuperMarketDao();
-		custDao = new CustomerDao();
-		movieDao = new MovieDao();
+		empProcessor = new EmployeeDataProcessor(scanner, empDao);
 	}
 
 	public static void main(String[] args) {
@@ -60,8 +56,6 @@ public class JdbcDataDriver {
 
 			System.out.println("###### " + DriverTypes.EMPLOYEE.getName().toUpperCase() + " Processor Starts ######");
 
-			System.out.println("##### " + DriverTypes.EMPLOYEE.getName().toUpperCase() + "Processor Starts #####");
-
 			Arrays.stream(OperationTypes.values()).forEach(eachOperation -> {
 				System.out.println(eachOperation.getOperationType() + " : " + eachOperation.getOperationName());
 			});
@@ -73,22 +67,26 @@ public class JdbcDataDriver {
 				System.out.println("How Many Records do you want to insert ? :");
 				int employeeCount = scanner.nextInt();
 
-				EmployeeDataProcessor emp = new EmployeeDataProcessor(scanner, empDao);
+				EmployeeDataProcessor empProcessor = new EmployeeDataProcessor(scanner, empDao);
 				int empCount = 1;
 				for (int i = 0; i < employeeCount; i++) {
-					emp.process(empCount);
+					empProcessor.insertProcess(empCount);
 					empCount++;
 				}
 
-				System.out.println(employeeCount + "records inserted sucessfully");
-
-				System.out.println(employeeCount + "records inserted sucessfully");
-
-				System.out.println(employeeCount + "records inserted sucessfully");
-
 			} else if (operationTypeEmployee == 2) {
 				System.out.println("fetching employee records from the database");
-				empDao.employeeSelectData();
+				empDao.selectEmployeeData();
+				
+			} else if (operationTypeEmployee == 3) {  
+			    System.out.println("Updating employee records...");
+			    empProcessor.updateProcess();
+
+			}else if (operationTypeEmployee == 4) { 
+			    System.out.println("Deleting employee records...");
+			    empProcessor.deleteProcess();
+			} else {
+			    System.out.println("Invalid operation type selected.");
 			}
 			System.out.println("###### " + DriverTypes.EMPLOYEE.getName().toUpperCase() + " Processor ends ######");
 
@@ -179,7 +177,6 @@ public class JdbcDataDriver {
 			break;
 
 		case 5:
-
 			System.out.println("##### " + DriverTypes.MOBILE.getName().toUpperCase() + "Processor Starts  ######");
 			Arrays.stream(OperationTypes.values()).forEach(eachOperation -> {
 				System.out.println(eachOperation.getOperationType() + " : " + eachOperation.getOperationName());
@@ -189,44 +186,20 @@ public class JdbcDataDriver {
 			int operationTypeMobile = scanner.nextInt();
 
 			if (operationTypeMobile == 1) {
-
-				System.out.println("##### " + DriverTypes.MOBILE.getName().toUpperCase() + "Processor Starts  ######");
-				Arrays.stream(OperationTypes.values()).forEach(eachOperation -> {
-					System.out.println(eachOperation.getOperationType() + " : " + eachOperation.getOperationName());
-				});
-
-				System.out.println("Please choose the operation type: ");
-				int operationTypeMobiles = scanner.nextInt();
-
-				if (operationTypeMobile == 1) {
-
-					System.out.println("How many records do you want to insert ? : ");
-					int Count = scanner.nextInt();
-
-					MobileDataProcessor mobileDataProcessor = new MobileDataProcessor(scanner, mobileDao);
-					int rowcount = 1;
-					for (int i = 0; i < Count; i++) {
-						mobileDataProcessor.process(rowcount);
-						rowcount++;
-					}
-				} else if (operationTypeMobile == 2) {
-					System.out.println("fetching mobile records from database!!!!");
-					mobileDao.SelectData();
-				}
-
-				System.out.println("How many records do you want to insert ? : ");
+				System.out.println("How Many Records do you want to insert ? :");
 				int Count = scanner.nextInt();
-
 				MobileDataProcessor mobileDataProcessor = new MobileDataProcessor(scanner, mobileDao);
 				int rowcount = 1;
 				for (int i = 0; i < Count; i++) {
 					mobileDataProcessor.process(rowcount);
 					rowcount++;
 				}
-			} else if (operationTypeMobile == 2) {
-
-			}
-			System.out.println("###### " + DriverTypes.MOBILE.getName().toUpperCase() + "Processor ends ######");
+				} else if (operationTypeMobile == 2) {
+					System.out.println("fetching mobile records from database!!!!");
+					mobileDao.SelectData();
+				}
+				System.out.println("###### " + DriverTypes.MOBILE.getName().toUpperCase() + " Processor ends ######");
+			
 		default:
 
 			break;
--- src/main/java/com/khadri/jdbc/prepare/data/employee/dao/EmployeeDao.java
+++ src/main/java/com/khadri/jdbc/prepare/data/employee/dao/EmployeeDao.java
@@ -10,12 +10,14 @@ import com.khadri.jdbc.prepare.connection.JdbcConnectionUtil;
 import com.khadri.jdbc.prepare.data.employee.dao.model.Employee;
 
 public class EmployeeDao {
-	PreparedStatement pstmt=null;
+	PreparedStatement pstmt;
 	Statement stmt;
-	public  void insertData(Employee emp)  {
+	Connection con=null;
+
+	public void insertEmployeeData(Employee emp) {
 		System.out.println("EmployeeDao : employeeData(-) starts");
-		try {   
-			Connection con = JdbcConnectionUtil.getConnection();
+		try {
+			Connection con=JdbcConnectionUtil.getConnection();
 			pstmt = con.prepareStatement("insert into Employee values(?,?,?,?)");
 			pstmt.setInt(1, emp.getId());
 			pstmt.setString(2, emp.getName());
@@ -24,27 +26,69 @@ public class EmployeeDao {
 			pstmt.executeUpdate();
 
 		} catch (SQLException e) {
-			System.out.println("SQLException occours:"+ e);
-		}
-		finally {
-			   JdbcConnectionUtil.closeResources(); 
-				System.out.println("EmployeeDao : employeeData(-) ends");
+			System.out.println("SQLException occours:" + e);
+		} finally {
+			JdbcConnectionUtil.closeResources();
+			System.out.println("EmployeeDao : employeeData(-) ends");
 		}
 	}
-     public void employeeSelectData() {
-    	 try {
-    		 Connection con = JdbcConnectionUtil.getConnection();
-    		stmt= con.createStatement();
-    		 ResultSet resultSet = stmt.executeQuery("select * from Employee");
-    		 while (resultSet.next()) {
-    			 System.out.println(resultSet.getInt(1) + "-" + resultSet.getString(2) + "-" + resultSet.getString(3) + "-"+resultSet.getDouble(4));
-    		 }
-    	 }catch(SQLException sqle){
-    		 System.out.println(" EmployeeDao SQLException occurs");
-    	 }finally {
-    		 JdbcConnectionUtil.closeResources();
-    	 }
-     }
-}
-     
 
+	public void selectEmployeeData() {
+		try {
+			Connection con=JdbcConnectionUtil.getConnection();
+			stmt = con.createStatement();
+			ResultSet resultSet = stmt.executeQuery("select * from Employee");
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt(1) + "-" + resultSet.getString(2) + "-" + resultSet.getString(3)
+						+ "-" + resultSet.getDouble(4));
+			}
+		} catch (SQLException sqle) {
+			System.out.println(" EmployeeDao SQLException occurs");
+		} finally {
+			JdbcConnectionUtil.closeResources();
+		}
+	}
+
+	public boolean employeeUpdateData(Employee emp) {
+ 	    System.out.println("EmployeeDao : employeeUpdateData(-) starts");
+ 	    try {
+ 	    	 con=JdbcConnectionUtil.getConnection();
+ 	    	PreparedStatement  pstmt = con.prepareStatement("UPDATE employee SET name = ?, desigination = ?, salary = ? WHERE id = ?");
+ 			pstmt.setString(1, emp.getName());
+ 			pstmt.setString(2, emp.getDesigination());
+ 			pstmt.setDouble(3, emp.getSalary());
+ 			pstmt.setInt(4, emp.getId());
+
+ 	        int rowsAffected = pstmt.executeUpdate();
+ 	        System.out.println( "Rows affected: " + rowsAffected);
+ 	    
+ 	    } catch (SQLException e) {
+ 	        System.out.println("SQLException occurs in employeeUpdateData: " + e);
+ 	    } finally {
+ 	        JdbcConnectionUtil.closeResources();
+ 	        System.out.println("EmployeeDao : employeeUpdateData(-) ends");
+ 	}
+ 	    return true;
+  }
+
+
+	public boolean employeeDeleteData(int employeeId) {
+    	    System.out.println("EmployeeDao : employeeDeleteData(-) starts");
+    	    try {
+    	    	Connection con=JdbcConnectionUtil.getConnection();
+    	        pstmt = con.prepareStatement("delete from employee where id = ?");
+    	        pstmt.setInt(1, employeeId);
+
+    	        int rowsAffected = pstmt.executeUpdate();
+    	        System.out.println("Rows affected: " + rowsAffected);
+
+    	    } catch (SQLException e) {
+    	        System.out.println("SQLException occurs in employeeDeleteData: " + e);
+    	    } finally {
+    	        JdbcConnectionUtil.closeResources();
+    	        System.out.println("EmployeeDao : employeeDeleteData(-) ends");
+    	}
+    	    return true;
+    	}
+
+}
