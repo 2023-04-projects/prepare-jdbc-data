5fda158e56716e59134dbf9e059fedf7f550a944
thasmiyas39@gmail.com <Thasmiya@gmail.com>
2024-09-23 21:08:39 -1200

    removed file

--- src/main/java/com/khadri/jdbc/prepare/data/driver/JdbcDataDriver.java
+++ src/main/java/com/khadri/jdbc/prepare/data/driver/JdbcDataDriver.java
@@ -18,8 +18,6 @@ import com.khadri.jdbc.prepare.data.supermarket.processor.SuperMarketDataProcess
 
 public class JdbcDataDriver {
 	private Scanner scanner;
-
-	private CustomerDataProcessor custDataProcessor;
 	private EmployeeDao empDao;
 	private CustomerDao custDao;
 	private MobileDao mobileDao;
@@ -35,14 +33,14 @@ public class JdbcDataDriver {
 		scanner = new Scanner(System.in);
 		empDao = new EmployeeDao();
 		mobileDao = new MobileDao();
+		custDao = new CustomerDao();
 		superMarketDao = new SuperMarketDao();
 		movieDao = new MovieDao();
 		mobileDataProcessor = new MobileDataProcessor(scanner, mobileDao);
 		custProcessor = new CustomerDataProcessor(scanner, custDao);
 		custDao = new CustomerDao();
-		custDataProcessor = new CustomerDataProcessor(scanner, custDao);
 		movieDataProcesser = new MovieDataProcesser(scanner, movieDao);
-		employeeDataProcessor = new EmployeeDataProcessor(scanner, empDao);
+		 employeeDataProcessor = new EmployeeDataProcessor(scanner, empDao);
 
 	}
 
@@ -117,18 +115,17 @@ public class JdbcDataDriver {
 				int custRowCount = 1;
 				for (int i = 0; i < custCount; i++) {
 					custProcessor.insertProcess(custRowCount);
-					custDataProcessor.insertProcess(custRowCount);
 					custRowCount++;
 				}
 			} else if (operationTypeCustomer == 2) {
 				System.out.println("fetching customer records from the database...");
-				custDataProcessor.selectProcess(operationTypeCustomer);
+				custDao.selectCustomerData();
 			} else if (operationTypeCustomer == 3) {
 				System.out.println("Updating customer records...");
-				custDataProcessor.updateProcess();
+				custProcessor.updateProcess();
 			} else if (operationTypeCustomer == 4) {
 				System.out.println("Deleting customer records...");
-				custDataProcessor.deleteProcess();
+				custProcessor.deleteProcess();
 			} else {
 				System.out.println("Invalid operation type selected.");
 			}
@@ -181,6 +178,7 @@ public class JdbcDataDriver {
 					movieDataProcesser.insertProcess(recordCount);
 					recordCount++;
 				}
+
 			} else if (operationType == 2) {
 				System.out.println("freching movie databace records");
 				movieDataProcesser.selectProcess(operationType);
@@ -205,7 +203,6 @@ public class JdbcDataDriver {
 			int operationTypeMobile = scanner.nextInt();
 
 			if (operationTypeMobile == 1) {
-
 				System.out.println("How Many Records do you want to insert ? :");
 				int Count = scanner.nextInt();
 				MobileDataProcessor mobileDataProcessor = new MobileDataProcessor(scanner, mobileDao);
@@ -227,8 +224,8 @@ public class JdbcDataDriver {
 			} else {
 				System.out.println("invalid operation type selected!!!");
 			}
-			System.out.println("###### " + DriverTypes.MOBILE.getName().toUpperCase() + " Processor ends ######");
-
+				System.out.println("###### " + DriverTypes.MOBILE.getName().toUpperCase() + " Processor ends ######");
+	
 			break;
 
 		}
